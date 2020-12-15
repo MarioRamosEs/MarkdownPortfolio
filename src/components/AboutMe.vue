76c22fd3c56fabe100f39ace1a296cecd2b94545
Mario Ramos <mariodarken@gmail.com>
2020-12-15 13:25:22 +0100

    About me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,12 +1,83 @@
 <template>
-  <div class="post-card content-box">
-    <div class="post-card__content">
-      <h2>My Stacks</h2>
-      <img src="/images/Stacks.png" alt="">
+  <div>
+    <div class="post-card content-box">
+      <div class="post-card__content">
+        <h2 class="post-card__title">About Me</h2>
+        <p>
+          Young FullStack Developer focused in .NET Core, Vue, React Native and
+          Azure. Although I also like to program video games and other things in
+          my spare time. Eager to learn more every day. 👨‍💻
+        </p>
+        <p>
+          Most of the projects I have worked for are private, but here I show
+          some of the ones that are public. Currently looking for a job in
+          Barcelona 😉
+        </p>
+      </div>
+    </div>
+
+    <div class="post-card content-box">
+      <div class="post-card__content">
+        <h2 class="post-card__title">Tech Stacks</h2>
+        <p class="post-card__description" v-if="false">HOLA</p>
+        <img src="/images/Stacks.png" alt="Tech Stacks" />
+      </div>
+    </div>
+
+    <div class="post-card content-box">
+      <div class="post-card__content">
+        <h2 class="post-card__title">Work experience</h2>
+
+        <v-timeline :reverse="reverse" dense>
+          <v-timeline-item>
+            <template v-slot:icon>
+              <v-avatar>
+                <img src="/images/AtInfoserveis_logo.jpg" />
+              </v-avatar>
+            </template>
+            <v-card class="elevation-2">
+              <v-card-title class="headline">AT Infoserveis</v-card-title>
+              <v-card-subtitle>(sept. 2018 - nov. 2020)</v-card-subtitle>
+              <v-card-text>
+                <ul>
+                  <li>
+                    Development of customized platforms. Websites and cross-platform apps.
+                  </li>
+                  <li>
+                    Development of integrations with A3ERP. Synchronizers, websites, apps and plant management software.
+                  </li>
+                  <li>
+                    Development of integrations with Prestashop, Salesforce, Zoho, WooComerce and Consentio.
+                  </li>
+                  <li>
+                    Online and face-to-face assistance for any problem.
+                  </li>
+                </ul>
+              </v-card-text>
+            </v-card>
+          </v-timeline-item>
+          <v-timeline-item>
+            <template v-slot:icon>
+              <v-avatar>
+                <img src="/images/SLS_logo.jpg" />
+              </v-avatar>
+            </template>
+            <v-card class="elevation-2">
+              <v-card-title class="headline">Serendipity Learning Services</v-card-title>
+              <v-card-subtitle>(sept. 2015 - jun. 2018)</v-card-subtitle>
+              <v-card-text>
+                <ul>
+                  <li>Collaborating in the creation of STEM projects related to web, robotics, IoT and video games.</li>
+                </ul>
+              </v-card-text>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+      </div>
     </div>
   </div>
 </template>
-
+ˆ
 <script>
 export default {};
 </script>
