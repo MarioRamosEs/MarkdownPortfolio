8cbf18010513a34a6eaec796be217fa95d2045d0
Mario Ramos Montesinos <mariodarken@gmail.com>
2024-06-11 17:09:53 +0200

    Remove CV 2022 button

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -48,16 +48,6 @@
         Twitter
       </v-btn>
 
-      <v-btn
-        outlined
-        class="ma-2"
-        color="primary"
-        target="_blank"
-        href="CV_MarioRamos_2022.pdf"
-      >
-        <v-icon left> mdi-file-pdf-box </v-icon>
-        CV 2022
-      </v-btn>
     </v-row>
   </div>
 </template>
